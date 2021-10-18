b8ac524285725b00abb59b1b48f3952074783a71
Odeh Adejoh <42589643+stradox4u@users.noreply.github.com>
2021-10-18 18:28:59 +0100

    added an exclude option to the typescript compiler so it stopped complaining about the position of my test folder

--- dist/src/utils/cookieHelpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getExpiry = void 0;
-const getExpiry = () => {
-    const date = new Date();
-    const expiration = new Date(date.setDate(date.getDate() + 7));
-    return expiration;
-};
-exports.getExpiry = getExpiry;
